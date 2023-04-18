366e111c7399c88954d695f6609ed705cc81eacc
dieguitorocha <100223185+dieguitorocha@users.noreply.github.com>
2023-04-18 17:43:47 +0100

    World Design- Tilemaps- Completo

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -13,8 +13,11 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.5f * horizontal * Time.deltaTime;
+        position.y = position.y + 0.5f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
